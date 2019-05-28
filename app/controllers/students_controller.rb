6b48ac17bae243c7adae5d8c86ed93d86d309b00
Shimol-Khan <se.shimolkhan@gmail.com>
2019-05-28 08:25:49 +0800

    Fixed Student Model

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -57,6 +57,6 @@ class StudentsController < ApplicationController
     end
 
     def student_params
-      params.require(:student).permit(:user_id, :matric_id, :name, :student_status, :study_level, :current_semester, :student_type, :college, :ic_number, :passport_number, :permanent_address, :current_address, :country, :gender, :date_of_birth, :mobile_number, :blood_type, :race, :religion, :marital_status, :bank, :bank_account, :sponsor, :siswa_mail, :personal_email, :cgpa_1, :cgpa_2, :cgpa_3, :cgpa_4, :cgpa_5, :cgpa_6, :cgpa_7, :cgpa_8, :faculty_id, :program_id)
+      params.require(:student).permit(:user_id, :matric_id, :name, :student_status, :study_level, :current_semester, :student_type, :college, :ic_number, :passport_number, :permanent_address, :current_address, :country, :gender, :date_of_birth, :mobile_number, :blood_type, :race, :religion, :marital_status, :bank, :bank_account, :sponsor, :siswa_email, :personal_email, :cgpa_1, :cgpa_2, :cgpa_3, :cgpa_4, :cgpa_5, :cgpa_6, :cgpa_7, :cgpa_8, :faculty_id, :program_id)
     end
 end
